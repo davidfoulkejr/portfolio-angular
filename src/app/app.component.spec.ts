f5ebf7e98a14978d6325b2c5f1e4ac7ceabc78cc
David Foulke <dfoulke@deloitte.com>
2019-08-06 11:53:48 -0400

    changed repo name and favicon

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'portfolio-angular'`, () => {
+  it(`should have as title 'resume-portfolio'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('portfolio-angular');
+    expect(app.title).toEqual('resume-portfolio');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to portfolio-angular!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to resume-portfolio!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'portfolio-angular';
+  title = 'resume-portfolio';
 }
