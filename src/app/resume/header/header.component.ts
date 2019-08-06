3dcb273df726867f950cb3c403a421422c33a017
David Foulke <dfoulke@deloitte.com>
2019-08-06 17:17:38 -0400

    saving progress

--- src/app/resume/header/header.component.ts
+++ src/app/resume/header/header.component.ts
@@ -2,30 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-resume-header',
-  template: `
-    <div class="header__container">
-      <div class="header__content">
-        <div>
-          <h2 class="header__title">David Foulke</h2>
-        </div>
-        <div class="flex-spacer"></div>
-        <div>
-          <ul class="header__actions">
-            <li class="header__action-button">
-              <app-icon-button source="gmail" fill="#fff"></app-icon-button>
-            </li>
-            <li class="header__action-button">
-              <app-icon-button source="github" fill="#fff"></app-icon-button>
-            </li>
-            <li class="header__action-button">
-              <app-icon-button source="linkedin" fill="#fff"></app-icon-button>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-    <div></div>
-  `,
+  templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {}
--- src/app/resume/resume.module.ts
+++ src/app/resume/resume.module.ts
@@ -3,10 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ResumeRoutingModule } from './resume-routing.module';
 import { ResumeComponent } from './resume.component';
 import { HeaderComponent } from './header/header.component';
-import { IconButtonComponent } from './icon-button/icon-button.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [ResumeComponent, HeaderComponent, IconButtonComponent],
+  declarations: [ResumeComponent, HeaderComponent, FooterComponent],
   imports: [ResumeRoutingModule, CommonModule]
 })
 export class ResumeModule {}
