5e07e838f1de301131ded7e33c83d93725c90377
David Foulke <dfoulke@deloitte.com>
2019-08-05 22:09:54 -0400

    added a bunch of stuff idk

--- src/app/resume/header/header.component.ts
+++ src/app/resume/header/header.component.ts
@@ -4,12 +4,12 @@ import { Component } from '@angular/core';
   selector: 'app-resume-header',
   template: `
     <div class="header__container">
-      <div class="header__content content-container">
-        <div class="header__item">
+      <div class="header__content">
+        <div>
           <h2 class="header__title">David Foulke</h2>
         </div>
-        <div class="header__spacer"></div>
-        <div class="header__item">
+        <div class="flex-spacer"></div>
+        <div>
           <ul class="header__actions">
             <li class="header__action-button">
               <app-icon-button source="gmail" fill="#fff"></app-icon-button>
@@ -26,6 +26,6 @@ import { Component } from '@angular/core';
     </div>
     <div></div>
   `,
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {}
